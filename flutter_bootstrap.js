83071d53a3fd01f9bb4f082e230b9d01cf15237b
akaba <akaba@sanyukk.co.jp>
2026-09-25 14:24:38 +0900

    Update web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2336597055" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2776696795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
